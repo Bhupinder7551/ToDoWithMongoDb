298bc85445727feb7aa7c4f5297fd0834a8072fd
Bhupinder Singh Sandhu <bhupindersingh7551@gmail.com>
2020-04-24 15:32:30 +1200

    react_helloWorld

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 
 class App extends Component {
-  render() {
-    console.log("Host URL"+process.env.PUBLIC_URL);
-    return (
+    render() {
+        return (
 
-      <Router basename={process.env.PUBLIC_URL}>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
-        </header>
-          <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
-          </Switch>
-      </div>
-    </Router>
-    );
-  }
+            <Router basename={process.env.PUBLIC_URL}>
+                <div className="App">
+                 
+                    <Switch>
+                        <h1>hello world</h1>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
